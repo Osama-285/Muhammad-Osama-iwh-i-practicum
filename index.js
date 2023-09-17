50715865859b0a67ae37a531dcfe0a263b11901d
Muhammad Osama <osamaaslam285@gmail.com>
2023-09-17 19:08:36 +0500

    Updated Code 1

--- index.js
+++ index.js
@@ -1,58 +1,22 @@
 const express = require("express");
 const axios = require("axios");
 const app = express();
-
+const bodyParser = require("body-parser");
 app.set("view engine", "pug");
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.static("css"));
 // * Please includethe private app access token in your repo BUT only an access token built in a TEST ACCOUNT. Don't do this practicum in your normal account.
 const PRIVATE_APP_ACCESS = "";
 
-/** 
-
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
+app.get("/update", (req, res) => {
+  res.render("form"); // Render the form.pug template
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
-
+app.post("/submit", (req, res) => {
+  const formData = req.body; // Access form data here
+  console.log("Form Data", formData);
+  res.redirect("/update");
 });
-*/
-
-// * Localhost
 app.listen(3000, () => console.log("Listening on http://localhost:3000"));
