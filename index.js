8fea3431b2d93e0508f8784a28ba8548a87c5dcf
Muhammad Osama <osamaaslam285@gmail.com>
2023-09-17 19:43:16 +0500

    New Table and Form Added

--- index.js
+++ index.js
@@ -10,6 +10,23 @@ app.use(express.static("css"));
 // * Please includethe private app access token in your repo BUT only an access token built in a TEST ACCOUNT. Don't do this practicum in your normal account.
 const PRIVATE_APP_ACCESS = "";
 
+app.get("/", async (req, res) => {
+  const contacts =
+    "https://api.hubapi.com/crm/v3/objects/contacts?properties=email,middle_name,firstname";
+  const headers = {
+    Authorization: `Bearer pat-na1-73aca9b3-98ba-4f9c-a9ff-899ceb30a905`,
+    "Content-Type": "application/json",
+  };
+
+  try {
+    const resp = await axios.get(contacts, { headers });
+    const data = resp.data.results;
+    res.render("contacts", { title: "Contact Table", data });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
 app.get("/update", (req, res) => {
   res.render("form"); // Render the form.pug template
 });
@@ -17,6 +34,6 @@ app.get("/update", (req, res) => {
 app.post("/submit", (req, res) => {
   const formData = req.body; // Access form data here
   console.log("Form Data", formData);
-  res.redirect("/update");
+  res.redirect("/");
 });
 app.listen(3000, () => console.log("Listening on http://localhost:3000"));
